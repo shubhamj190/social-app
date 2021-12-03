4e84b1eb61f7661b7c9f7fe3f42b20e21095f9cb
shubham j <shubham.jadhav@ufaber.com>
2021-12-03 10:10:52 +0530

    bug fixed3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express= require('express')
 const app = express()
 const format= require('date-format')
-const port= procewss.env.PORT || 5000
+const port= process.env.PORT || 5000
 
 app.get('/', (req, res)=>{
     res.status(202).send('hello from shubham')
